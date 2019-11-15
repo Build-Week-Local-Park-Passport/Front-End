7e33b5686a542125ac17e9198aaf305235d3940c
CLYPR Technologies <cbanks@clypr.co>
2019-11-14 20:45:46 -0500

    display ParkList on homepage

--- parkpass-front-end/src/components/Parks/ParkCard.js
+++ parkpass-front-end/src/components/Parks/ParkCard.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import Card from '@material-ui/core/Card'
+import { makeStyles } from '@material-ui/core/styles';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-export default function ParkCard() {
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+});
+
+export default function ParkCard( { name, location, description }) {
+  const classes = useStyles();
+
   return (
-    <div>
-      Park Card
-    </div>
-  )
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Location: {location}
+            <br></br>
+            Description: {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
--- parkpass-front-end/src/components/Parks/ParkList.js
+++ parkpass-front-end/src/components/Parks/ParkList.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ParkCard from "./ParkCard";
 
 export default function ParkList() {
+  // TODO: Add useState to track data from useEffect
+
+  const [park, setPark] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('https://park-passport.herokuapp.com/api/parks')
+    .then((response) => {
+      setPark(response.data);
+      console.log(response);
+    })
+    .catch(error => {
+      console.error('Server Error', error);
+    });
+  }, []);
+
   return (
-    <div>
-      ParkList
-    </div>
-  )
+    <section>
+
+      {park.map(parks => (
+        <ParkCard 
+        key={parks.id} 
+        name={parks.name} 
+        description={parks.description} 
+        location={parks.location} 
+        />
+      ))}
+    </section>
+  );
+
 }
