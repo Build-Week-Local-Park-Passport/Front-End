5b9b99530af3228ba4f35862a463f79e6ff827c0
Clif <clifhodges13@gmail.com>
2019-11-20 19:47:58 -0600

    update production build

---
+++ parkpass-front-end/build/precache-manifest.940a69ea0dac3f124ebe3b8c89dac1cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b42656cb8a012d5915785866df7358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f113934c75af02d08a8f",
+    "url": "/static/css/main.38d63af6.chunk.css"
+  },
+  {
+    "revision": "f103f5c0185a51656768",
+    "url": "/static/js/2.aefd4fa0.chunk.js"
+  },
+  {
+    "revision": "f113934c75af02d08a8f",
+    "url": "/static/js/main.00e99dde.chunk.js"
+  },
+  {
+    "revision": "47e824c4dd7573aa438b",
+    "url": "/static/js/runtime-main.8ea2ca45.js"
+  }
+]);
--- parkpass-front-end/build/service-worker.js
+++ parkpass-front-end/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f0a360ba60da03e23dd3312efa60b7a8.js"
+  "/precache-manifest.940a69ea0dac3f124ebe3b8c89dac1cf.js"
 );
 
 self.addEventListener('message', (event) => {
