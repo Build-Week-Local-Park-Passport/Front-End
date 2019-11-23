3d891ac13edd2a00d7ec55014c34422620bbdd04
Clif <clifhodges13@gmail.com>
2019-11-23 14:08:22 -0600

    added updated build folder for deployment

---
+++ parkpass-front-end/build/precache-manifest.d076b061f7f0d4ea2a9c8f8d14bb6a9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f666d3cd57fa3e14e70181001e8faf89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e1d978d9623597a2001",
+    "url": "/static/css/main.fee0a314.chunk.css"
+  },
+  {
+    "revision": "072a012136213c4e09c0",
+    "url": "/static/js/2.0991b6fa.chunk.js"
+  },
+  {
+    "revision": "6e1d978d9623597a2001",
+    "url": "/static/js/main.2b876ffe.chunk.js"
+  },
+  {
+    "revision": "47e824c4dd7573aa438b",
+    "url": "/static/js/runtime-main.8ea2ca45.js"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "911920eae30d0aefa6b8cd77e7e530f0",
+    "url": "/static/media/Parkpass.911920ea.png"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/static/media/Raleway-Regular.9942588a.ttf"
+  }
+]);
